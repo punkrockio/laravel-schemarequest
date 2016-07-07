111362d4e4b6dc17aa0766dc1bb27aaba44f980e
Aaron Bullard <jabullard@aol.com>
2016-07-07 19:24:41 +0000

    Added test coverage

--- tests/stubs/PersonFormRequest.php
+++ tests/stubs/PersonFormRequest.php
@@ -1,18 +1,8 @@
-# laravel-schemarequest
-Server side form validation using JSON Schema
+<?php 
 
-## How to use
-1. Create a form request class as you would with artisan: php artisan make:request PersonRequest
-2. Change the extended class in PersonRequest to use SchemaRequest/SchemaFormRequest
-3. Implement the rules() method to return a JSON Schema string
-
-Example
-```
-<?php
-
-namespace App\Http\Requests;
 
 use SchemaRequest\SchemaFormRequest as Request;
+
 
 class PersonFormRequest extends Request
 {
@@ -46,5 +36,3 @@ class PersonFormRequest extends Request
         }';
     }
 }
-
-```
